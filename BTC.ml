3729b7fd6a28986e4ae0bec508c029e1c652edf6
Klafyvel <klafyvel@gmail.com>
2016-06-05 17:31:48 +0200

    Utilisation de float pour BTC.

--- BTC.ml
+++ BTC.ml
@@ -21,12 +21,12 @@ let graphe = [|
 let n = 6;;
 
 let betweenness g n =
-  let cB = Array.make n 0 in
+  let cB = Array.make n 0.0 in
   for s = 0 to n-1 do
     let stack = Stack.create() in
     let p = Array.make n [] in
-    let sigma = Array.make n 0 in
-    sigma.(s) <- 1;
+    let sigma = Array.make n 0.0 in
+    sigma.(s) <- 1.0;
     let d = Array.make n (-1) in
     d.(s) <- 0;
     let q = Queue.create() in
@@ -42,16 +42,16 @@ let betweenness g n =
             d.(w) <- d.(v) + 1;
           end;
           if d.(w) = (d.(v) + 1) then begin
-            sigma.(w) <- sigma.(w) + sigma.(v);
+            sigma.(w) <- sigma.(w) +. sigma.(v);
             p.(w) <- v::p.(w);
           end;
       done;
     done;
-    let delta = Array.make n 0 in
+    let delta = Array.make n 0.0 in
     while not (Stack.is_empty stack) do
       let w = Stack.pop stack in
-      List.iter (fun v -> delta.(v) <- delta.(v) + sigma.(v) / sigma.(w) * (1 + delta.(w))) p.(w);
-      if w != s then begin cB.(w) <- cB.(w) + delta.(w); end;
+      List.iter (fun v -> delta.(v) <- delta.(v) +. sigma.(v) /. sigma.(w) *. (1.0 +. delta.(w))) p.(w);
+      if w != s then begin cB.(w) <- cB.(w) +. delta.(w); end;
     done;
   done;
   cB
